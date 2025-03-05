a8ed4efd004fd881bc08a5e0c82659e484c8c582
Killian1337 <killian.berthier@viacesi.fr>
2025-03-05 13:34:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.getElementById('backButton').addEventListener('click', function() {
-    window.history.back();
-});
+document.addEventListener("DOMContentLoaded", function(){
+    const cookieBanner = document.getElementById("cookie-banner")
+    const acceptButton = document.getElementById("accept-cookies")
+
+    /*if(localStorage.getItem("cookiesAccepted") === 'true'){
+      cookieBanner.style.display = "none";  
+    }*/
+
+    acceptButton.addEventListener("click", function(){
+        localStorage.setItem("cookiesAccepted", "true");
+        cookieBanner.style.display = "none";
+    })
+})
