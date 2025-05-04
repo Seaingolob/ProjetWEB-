254705b60fe1ac2c0bc39318bab5fb36bb93252e
Samuel Ceccarelli <samuel.ceccarelli@gmail.com>
2025-05-04 21:06:09 +0200

    Add files via upload

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController {
     }
 
     private function checkAuthentication() {
-        session_start();
+
         if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > 3600)) {
             session_unset();
             session_destroy();
